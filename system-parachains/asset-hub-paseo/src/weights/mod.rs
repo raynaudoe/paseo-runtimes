14e798d87b4d502c63cca0580a75709b63b6f48c
Hector Bulgarini <hbulgarini@gmail.com>
2024-05-06 10:30:41 +0200

    wip

--- system-parachains/asset-hub-paseo/src/weights/mod.rs
+++ system-parachains/asset-hub-paseo/src/weights/mod.rs
@@ -15,13 +15,18 @@
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
 pub mod block_weights;
+pub mod cumulus_pallet_dmp_queue;
+pub mod cumulus_pallet_parachain_system;
 pub mod cumulus_pallet_xcmp_queue;
 pub mod extrinsic_weights;
 pub mod frame_system;
+pub mod pallet_asset_conversion;
 pub mod pallet_assets_foreign;
 pub mod pallet_assets_local;
+pub mod pallet_assets_pool;
 pub mod pallet_balances;
 pub mod pallet_collator_selection;
+pub mod pallet_message_queue;
 pub mod pallet_multisig;
 pub mod pallet_nfts;
 pub mod pallet_proxy;
@@ -30,6 +35,7 @@ pub mod pallet_timestamp;
 pub mod pallet_uniques;
 pub mod pallet_utility;
 pub mod pallet_xcm;
+pub mod pallet_xcm_bridge_hub_router;
 pub mod paritydb_weights;
 pub mod rocksdb_weights;
 pub mod xcm;
